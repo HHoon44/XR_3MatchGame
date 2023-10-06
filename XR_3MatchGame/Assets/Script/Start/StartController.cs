93fa362b0df565cfa6fc944c88ccea335101a079
MeLeeTa <sonmet99@metaskyworld.net>
2023-10-06 11:11:26 +0900

    no message

---
+++ XR_3MatchGame/Assets/Script/Start/StartController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartController : MonoBehaviour
+{
+
+    public void PlayBtnFun()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+}
